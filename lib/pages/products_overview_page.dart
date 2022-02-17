50606522e64a85589ecd6132f60116a357740f81
jhonatasal <jhonatasa_s@hotmail.com>
2022-02-17 16:21:39 -0300

    criação de components

---
+++ lib/pages/products_overview_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:shop/components/product_item.dart';
+import 'package:shop/data/dummy_data.dart';
+import 'package:shop/models/product.dart';
+
+class ProductsOverviewPage extends StatelessWidget {
+  final List<Product> loadedProduct = dummyProducts;
+
+  ProductsOverviewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Minha Loja'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: GridView.builder(
+          itemCount: loadedProduct.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemBuilder: (ctx, i) => ProductItem(product: loadedProduct[i]),
+        ),
+      ),
+    );
+  }
+}
